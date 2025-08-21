f44dfc58bba0f97d8108181870297156a3911510
MANIKANDAN <161091781+Manikandan-E56@users.noreply.github.com>
2025-08-21 10:07:37 +0530

    Time: 14 ms (31.66%), Space: 45.1 MB (24.06%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length()==1){
+            return true;
+        }
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+       s= s.toLowerCase();
+        int r=0;
+        int l=s.length()-1;
+        while(r<l){
+            if(s.charAt(r)!=s.charAt(l))return false;
+            r++;
+            l--;
+        }
+        return true;
+    }
+}
