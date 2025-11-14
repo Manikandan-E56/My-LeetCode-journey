b9d3c4c157973496f448fc9c608e2125c171d37d
MANIKANDAN <161091781+Manikandan-E56@users.noreply.github.com>
2025-11-14 15:02:55 +0530

    Time: 1 ms (79.98%), Space: 42.6 MB (7.04%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int divide(int a, int b) {
+
+        if (a == Integer.MIN_VALUE && b == -1) {
+            return Integer.MAX_VALUE;
+        }
+        
+        return a/b;
+    }
+}
